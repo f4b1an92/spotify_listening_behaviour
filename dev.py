25f53a67ebb355d5aeb270e6780a814a505a2ca9
f4b1an92 <fabianstork123@gmail.com>
2025-12-19 13:21:08 +0100

    Updating to latest version of local repo

--- dev.py
+++ dev.py
@@ -3,9 +3,11 @@ import os
 from datetime import datetime, timedelta
 import yaml
 import webbrowser
+import numpy as np
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.exceptions import SpotifyException
 
 # Define main path (this should give the same path independant of the execution env (i.e. terminal, Jupyter NB or IDE))
 if sys.stdin.isatty() & sys.stdout.isatty():
@@ -29,6 +31,7 @@ with open(MAIN_PATH + '\\resources\\config.yaml', 'r') as f:
 
 CLIENT_ID = config['client_id']  # Needs to come from AWS Secrets Manager
 CLIENT_SECRET = config['client_secret']  # Needs to come from AWS Secrets Manager
+REFRESH_TOKEN = config['refresh_token']  # Needs to come from AWS Secrets Manager
 #REDIRECT_URI = config['redirect_uri']  # ToDo: Check which redirect URI can be used on AWS Lambda
 #REDIRECT_URI = 'https://2hrguq0is4.execute-api.eu-central-1.amazonaws.com/dev/API_gateway_test_get' # Redirect URI if you want to check the auth response from Spotify (JSON message)
 REDIRECT_URI = 'https://g35l66k9qj.execute-api.eu-central-1.amazonaws.com/get_auth_code'
@@ -43,10 +46,74 @@ sp_oauth = SpotifyOAuth(
     scope='user-read-private user-read-email user-read-recently-played',
     open_browser=False
 )
-token_info = sp_oauth.refresh_access_token(refresh_token='AQCTbBqX6fczhF5lfXg5YFKXqGEZAngRx0QY9P1jIsoGApfmYNaAkiH9uTo4ONRkfO62RmPV6dWoO4lG_zON99Qx1eGqOkNdrbodTVb-pdlPXPd4kBvipMJuckWoOesmh-Q')
-token_info['access_token']
+token_info = sp_oauth.refresh_access_token(refresh_token=REFRESH_TOKEN)
+access_token = token_info['access_token']
+
+# Use AccessToken to get recently played user track
+sp = spotipy.Spotify(auth=access_token)  # sp = spotipy.Spotify(auth_manager=sp_oauth)
+
+after_thr = ut.get_timestamp_after(offset=1)  # offset in days
+recently_played = sp.current_user_recently_played(
+    limit=50, after=after_thr, before=None
+)
 
 
+# Parse JSON to pandas.DataFrame
+data = ut.parse_recently_played_json(req=recently_played)
+print(data.shape)
+
+# Parse Artist Response
+artist_id_inputs = list(data['first_artist_id'].drop_duplicates().values)
+artists_response = sp.artists(artists=artist_id_inputs)
+
+artist_df = ut.parse_artists_json(req=artists_response)
+
+
+# Parse Album response
+album_id_inputs = list(data['album_id'].drop_duplicates().values)
+albums_response = sp.albums(albums=album_id_inputs)
+
+albums_df = ut.parse_albums_json(req=albums_response)
+
+# Parse track response
+track_id_inputs = list(data['track_id'].drop_duplicates().values)
+try:
+    track_response = sp.audio_features(tracks=track_id_inputs)
+    track_df = ut.parse_audio_features_json(req=track_response)
+except SpotifyException as err:
+    if err.http_status == 403:
+        track_df = pd.DataFrame({
+            'track_id': track_id_inputs
+            , 'acousticness': np.tile([np.nan], len(track_id_inputs))
+            , 'danceability': np.tile([np.nan], len(track_id_inputs))
+            , 'energy': np.tile([np.nan], len(track_id_inputs))
+            , 'key': np.tile([np.nan], len(track_id_inputs))
+            , 'instrumentalness': np.tile([np.nan], len(track_id_inputs))
+            , 'speechiness': np.tile([np.nan], len(track_id_inputs))
+            , 'loudness': np.tile([np.nan], len(track_id_inputs))
+            , 'tempo': np.tile([np.nan], len(track_id_inputs))
+            , 'valence': np.tile([np.nan], len(track_id_inputs))
+        })
+    else:
+        raise
+
+# Merge artist, album and track features with recently played tracks
+data = data.merge(artist_df, how='left', on='first_artist_id')
+data = data.merge(albums_df, how='left', on='album_id')
+data = data.merge(track_df, how='left', on='track_id')
+print(data.shape)
+
+# Restructure column order in final dataframe
+data = pd.concat([
+    data.loc[:, 'id':'track_popularity']
+    , data.loc[:, 'acousticness':'valence']
+    , data.loc[:, 'first_artist_id':'is_solo_artist']
+    , data.loc[:, 'artist_followers_total':'artist_image_width']
+    , data.loc[:, 'album_id':'album_total_tracks']
+    , data.loc[:, 'album_popularity':'album_image_width']
+    , data.loc[:, 'played_at':'after']
+], axis=1)
+print(data.shape)
 
 """
 auth_url = sp_oauth.get_authorize_url()
@@ -58,3 +125,4 @@ sp_oauth.get_authorization_code()
 sp_oauth._open_auth_url()
 sp_oauth.get
 """
+
--- utils.py
+++ utils.py
@@ -32,17 +32,21 @@ def parse_recently_played_json(req: json) -> pd.DataFrame:
                 artist_id_list.append(artist_id)
                 artist_name_list.append(artist_name)
 
+            result_dict['first_artist_id'] = artist_id_list[0]
             result_dict['combined_artist_id'] = "_".join(artist_id_list)
             if len(artist_name_list) == 1:
-                result_dict['is_solo_artist'] = True
+                result_dict['first_artist_name'] = artist_name_list[0]
                 result_dict['combined_artist_names'] = artist_name_list[0]
+                result_dict['is_solo_artist'] = True
             elif len(artist_name_list) == 2:
-                result_dict['is_solo_artist'] = False
+                result_dict['first_artist_name'] = artist_name_list[0]
                 result_dict['combined_artist_names'] = " & ".join(artist_name_list)
-            else:
                 result_dict['is_solo_artist'] = False
+            else:
+                result_dict['first_artist_name'] = artist_name_list[0]
                 artist_names = ", ".join(artist_name_list[:-1])
                 result_dict['combined_artist_names'] = " & ".join([artist_names, artist_name_list[-1]])
+                result_dict['is_solo_artist'] = False
 
         # album-loop
         for elem in ['id', 'album_type', 'name', 'release_date', 'total_tracks']:
@@ -70,3 +74,91 @@ def parse_recently_played_json(req: json) -> pd.DataFrame:
         'after']  # unix timestamp that indicates cutoff/offset from timestamp of data query (lower thr.)
 
     return result_df
+
+
+def parse_artists_json(req: json) -> pd.DataFrame:
+    """"""
+    artist_id_list = []
+    artist_followers_list = []
+    artist_popularity_list = []
+    artist_genres_list = []
+    artist_image_url_list = []
+    artist_image_height_list = []
+    artist_image_width_list = []
+
+    for i in range(len(req['artists'])):
+        artist_id_list.append(req['artists'][i]['id'])
+        artist_followers_list.append(req['artists'][i]['followers']['total'])
+        artist_popularity_list.append(req['artists'][i]['popularity'])
+        artist_image_url_list.append(req['artists'][i]['images'][0]['url'])
+        artist_image_height_list.append(req['artists'][i]['images'][0]['height'])
+        artist_image_width_list.append(req['artists'][i]['images'][0]['width'])
+        artist_genres = ", ".join(req['artists'][i]['genres'])
+
+        if artist_genres == '':
+            artist_genres = np.nan
+        artist_genres_list.append(artist_genres)
+
+    result_df = pd.DataFrame({
+        'first_artist_id': artist_id_list
+        , 'artist_followers_total': artist_followers_list
+        , 'artist_popularity': artist_popularity_list
+        , 'artist_genres': artist_genres_list
+        , 'artist_image_url': artist_image_url_list
+        , 'artist_image_height': artist_image_height_list
+        , 'artist_image_width': artist_image_width_list
+    })
+
+    return result_df
+
+
+def parse_albums_json(req: json) -> pd.DataFrame:
+    """"""
+    album_id_list = []
+    album_popularity_list = []
+    album_genres_list = []
+    album_image_url_list = []
+    album_image_height_list = []
+    album_image_width_list = []
+
+    for i in range(len(req['albums'])):
+        album_id_list.append(req['albums'][i]['id'])
+        album_popularity_list.append(req['albums'][i]['popularity'])
+        album_image_url_list.append(req['albums'][i]['images'][0]['url'])
+        album_image_height_list.append(req['albums'][i]['images'][0]['height'])
+        album_image_width_list.append(req['albums'][i]['images'][0]['width'])
+        album_genres = ", ".join(req['albums'][i]['genres'])
+        if album_genres == '':
+            album_genres = np.nan
+        album_genres_list.append(album_genres)
+
+    result_df = pd.DataFrame({
+        'album_id': album_id_list
+        , 'album_popularity': album_popularity_list
+        , 'album_genres': album_genres_list
+        , 'album_image_url': album_image_url_list
+        , 'album_image_height': album_image_height_list
+        , 'album_image_width': album_image_width_list
+    })
+
+    return result_df
+
+
+def parse_audio_features_json(req: json) -> pd.DataFrame:
+    """"""
+    result_df_list = []
+
+    for i in range(len(req)):
+        result_dict = {}
+        for feat in ['id', 'acousticness', 'danceability', 'energy', 'key', 'instrumentalness', 'speechiness', 'loudness',
+                     'tempo', 'valence']:
+            result_dict[feat] = [req[i][feat]]
+
+        temp_df = pd.DataFrame(result_dict)
+        result_df_list.append(temp_df)
+
+    result_df = pd.concat(result_df_list, axis=0).reset_index(drop=True)
+    result_df = result_df.rename(columns={'id': 'track_id'})
+
+    return result_df
+
